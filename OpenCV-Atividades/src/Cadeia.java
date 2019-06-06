c5446ec3eb3e95e38ffb3db75750e2d6b594dc21
Lucas M. Benossi <lucasbenossi@gmail.com>
2019-06-06 18:09:08 -0300

    cadeia

--- OpenCV-Atividades/src/Cadeia.java
+++ OpenCV-Atividades/src/Cadeia.java
@@ -1,7 +1,9 @@
 import org.opencv.core.Mat;
 
 public class Cadeia {
-	public static Mat cadeia2(Mat img) {
+	public static Mat cadeia(Mat img) {
+		Mat clone = img.clone();
+		
 		int startI = 0;
 		int startJ = 0;
 		
@@ -18,126 +20,45 @@ public class Cadeia {
 			}
 		}
 		
-		int i = 0;
-		int j = 0;
-		Direction direction = Direction.east;
+		int i = startI;
+		int j = startJ;
+		double[] color = {255,255,0};
+		clone.put(i, j, color);
+		
+		Direction anterior = Direction.east;
 		do {
-			for(int k = 0; k < 8; k++) {
-				Direction d = direction.getNextDirection();
-				double[] pixel = img.get(i + d.di, j + d.dj);
-				if(pixel[0] == 0) {
-					
+			Direction d = anterior;
+			do {
+				d = d.next();
+				if(img.get(i+d.i, j+d.j)[0] == 0) {
+					Direction d2 = d.next();
+					if(img.get(i+d2.i, j+d2.j)[0] == 255) {
+						break;
+					}
 				}
-			}
-		} while(i != startI && j != startJ);
-		
-		return img;
-	}
-	
-	public static Mat cadeia(Mat img) {
-		Mat mat = img.clone();
-		
-		Pixel start = null;
-		for(int i = 0; i < mat.rows(); i++) {
-			for(int j = 0; j < mat.cols(); j++) {
-				Pixel pixel = new Pixel(i, j, img.get(i, j));
-				if(pixel.isBlack()) {
-					start = pixel;
-					break;
-				}
-			}
-			if(start != null) {
-				break;
-			}
-		}
-		
-		Walker walker = new Walker(mat, start);
-		double[] color = {255, 255, 0};
-		int i = 0;
-		do {
-			walker.next();
-			Pixel p = walker.getPixel();
-			mat.put((int) p.i, (int) p.j, color);
-			i++;
-		} while(i < 100);
-		
-		return mat;
-	}
-	
-	private static class Walker {
-		private Mat mat;
-		private Pixel start;
-		private Pixel pixel;
-		private Direction direction;
-		
-		public Walker(Mat mat, Pixel start) {
-			super();
-			this.mat = mat;
-			this.start = start;
-			this.pixel = start;
-			this.direction = Direction.east;
-		}
-
-		public void next() {
-			Direction d = this.direction.getNextDirection();
-			while(!d.equals(this.direction)) {
-				int i = (int) pixel.i;
-				int j = (int) pixel.j;
-				
-				Pixel p1 = getPixelInDirection(mat, i, j, d.getNextDirection());
-				Pixel p2 = getPixelInDirection(mat, i, j, d);
-				
-				if(p1.isWhite() && p2.isBlack()) {
-					this.pixel = p2;
-					break;
-				}
-				
-				d = d.getNextDirection();
-			}
+			} while(!d.equals(anterior));
 			
-			System.out.println(String.format("%s %d %d", d.toString(), (int) this.pixel.i, (int) this.pixel.j));
-			this.direction = d;
-		}
+			i += d.i;
+			j += d.j;
+			clone.put(i, j, color);
+			System.out.println(d.ordinal());
+		} while(i != startI || j != startJ);
 		
-		public boolean hasNext() {
-			if(this.pixel.i == this.start.i && this.pixel.j == this.start.j) {
-				return false;
-			}
-			return true;
-		}
-		
-		public Pixel getPixel() {
-			return this.pixel;
-		}
-		
-		private Pixel getPixelInDirection(Mat mat, int i, int j, Direction direction) {
-			int ni = i + direction.di;
-			int nj = j + direction.dj;
-			double[] bgr = Utils.pixelOrNull(mat, ni, nj);
-			if(bgr == null) {
-				bgr = Pixel.bgrWhite;
-			}
-			return new Pixel(ni, nj, bgr);
-		}
+		return clone;
 	}
 	
 	private static enum Direction {
 		east(0, 1), northeast(-1, 1), north(-1, 0), northwest(-1, -1), west(0, -1), southwest(1, -1), south(1, 0), southeast(1, 1);
 		
-		int di, dj;
+		int i, j;
 		
-		private Direction(int di, int dj) {
-			this.di = di;
-			this.dj = dj;
+		private Direction(int i, int j) {
+			this.i = i;
+			this.j = j;
 		}
 		
-		public Direction getNextDirection() {
-			int i = this.ordinal();
-			i++;
-			if(i == 8) {
-				i = 0;
-			}
-			return Direction.values()[i];
+		public Direction next() {
+			return Direction.values()[(this.ordinal() + 1) % 8];
 		}
 	}
 	
--- OpenCV-Atividades/src/Main.java
+++ OpenCV-Atividades/src/Main.java
@@ -2,6 +2,6 @@
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		Imagem.load("cadeia").cadeia().show();
+		Imagem.load("cadeia").cadeia().zoomInQuadrado().zoomInQuadrado().show();
 	}
 }
